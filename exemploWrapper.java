fb68bcfa70d98dfaddb9551c0dd26325d87c32c3
eliel2703 <elielrodrigocordeiro@gmail.com>
2025-05-13 13:54:50 -0300

    13/05

--- exemploWrapper.java
+++ exemploWrapper.java
@@ -1,3 +1,18 @@
 public class exemploWrapper {
-    
+    public static void main(String[] args) {
+        Integer myInt = 100;
+
+        String myString = myInt.toString();
+
+        System.out.println("Comprimento da string: " + myString.length());
+
+        int primitiveInt = myInt.intValue();
+        System.out.println("Valor primitivo: " + primitiveInt);
+        
+        if (myInt % 2 == 0) {
+            System.out.println("O número é par!");
+        } else {
+            System.out.println("O número é ímpar!");
+        }
+    }
 }
