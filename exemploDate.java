8805a3fc985829540c86d2fe890049d7b48b06e5
eliel2703 <elielrodrigocordeiro@gmail.com>
2025-05-13 10:58:25 -0300

    13/05

--- exemploDate.java
+++ exemploDate.java
@@ -1,3 +1,18 @@
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
 public class exemploDate {
-    
+    public static void main(String[] args) {
+        // data atual
+        LocalDate hoje = LocalDate.now();
+
+        // data do aniversário
+        LocalDate aniversario = LocalDate.of(2026, 3, 27);
+
+        // calcular a diferença em dias
+        long diasRestantes = ChronoUnit.DAYS.between(hoje, aniversario);
+
+        System.out.println("Faltam " + diasRestantes + " dias.");
+
+    }
 }
