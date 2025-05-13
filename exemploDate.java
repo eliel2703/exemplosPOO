23929318915e50b3d561122e796c59dde8fd6fd8
eliel2703 <elielrodrigocordeiro@gmail.com>
2025-05-13 11:02:43 -0300

    13/05

--- exemploDate.java
+++ exemploDate.java
@@ -3,13 +3,8 @@ import java.time.temporal.ChronoUnit;
 
 public class exemploDate {
     public static void main(String[] args) {
-        // data atual
         LocalDate hoje = LocalDate.now();
-
-        // data do aniversário
         LocalDate aniversario = LocalDate.of(2026, 3, 27);
-
-        // calcular a diferença em dias
         long diasRestantes = ChronoUnit.DAYS.between(hoje, aniversario);
 
         System.out.println("Faltam " + diasRestantes + " dias.");
--- exemploEnum.java
+++ exemploEnum.java
@@ -1,3 +1,33 @@
 public class exemploEnum {
-    
+
+    public enum Sinal {
+        VERDE("Siga"),
+        AMARELO("Atenção"),
+        VERMELHO("Pare");
+        
+        private final String mensagem;
+
+        Sinal(String mensagem) {
+            this.mensagem = mensagem;
+        }
+
+        public String getMensagem() {
+            return mensagem;
+        }
+    }
+
+    public static void main(String[] args) {
+        Sinal sinalAtual = Sinal.VERDE;
+        
+        System.out.println("Sinal Atual: " + sinalAtual);
+        System.out.println("Mensagem: " + sinalAtual.getMensagem());
+
+        if (sinalAtual == Sinal.VERDE) {
+            System.out.println("Você pode seguir.");
+        } else if (sinalAtual == Sinal.AMARELO) {
+            System.out.println("Atenção, reduza a velocidade.");
+        } else if (sinalAtual == Sinal.VERMELHO) {
+            System.out.println("PARE!");
+        }
+    }
 }
