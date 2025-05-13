f26db54b10daca30fbd1a68be00c819c81e27da3
eliel2703 <elielrodrigocordeiro@gmail.com>
2025-05-13 11:03:20 -0300

    13/05

--- exemploEnum.java
+++ exemploEnum.java
@@ -1,4 +1,16 @@
 public class exemploEnum {
+    public static void main(String[] args) {
+        Sinal sinalAtual = Sinal.VERDE;
+        System.out.println("Sinal Atual: " + sinalAtual);
+        System.out.println("Mensagem: " + sinalAtual.getMensagem());
+        if (sinalAtual == Sinal.VERDE) {
+            System.out.println("Você pode seguir.");
+        } else if (sinalAtual == Sinal.AMARELO) {
+            System.out.println("Atenção, reduza a velocidade.");
+        } else if (sinalAtual == Sinal.VERMELHO) {
+            System.out.println("PARE!");
+        }
+    }
 
     public enum Sinal {
         VERDE("Siga"),
@@ -16,18 +28,4 @@ public class exemploEnum {
         }
     }
 
-    public static void main(String[] args) {
-        Sinal sinalAtual = Sinal.VERDE;
-        
-        System.out.println("Sinal Atual: " + sinalAtual);
-        System.out.println("Mensagem: " + sinalAtual.getMensagem());
-
-        if (sinalAtual == Sinal.VERDE) {
-            System.out.println("Você pode seguir.");
-        } else if (sinalAtual == Sinal.AMARELO) {
-            System.out.println("Atenção, reduza a velocidade.");
-        } else if (sinalAtual == Sinal.VERMELHO) {
-            System.out.println("PARE!");
-        }
-    }
 }
